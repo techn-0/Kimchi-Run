fbb19761bbb0e637e4ea3c1a73e77ea62208b4ae
Alisser <gnlrjswjd@naver.com>
2025-01-27 16:55:14 +0900

    점프 착지 애니메이션 적용

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     public Rigidbody2D PlayerRigidBody;
+    public Animator PlayerAnimator;
 
     private bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,12 +26,16 @@ public class Player : MonoBehaviour
             // PlayerRigidBody.linearVelocityY = 20;
             PlayerRigidBody.AddForceY(juumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            PlayerAnimator.SetInteger("state", 1);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.name == "Platform")
+        if(collision.gameObject.name == "Platform") // 플랫폼과 충돌하면
         {
+            if(!isGrounded){
+                PlayerAnimator.SetInteger("state",2);
+            }
             isGrounded = true;
         }
     }
