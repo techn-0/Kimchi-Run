9b14f256682538cd293fe7dc5ba119e2c3bb3829
Alisser <gnlrjswjd@naver.com>
2025-01-27 19:07:20 +0900

    건물 스포너

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Settings")]
+    public float minSPawnDelay;
+    public float maxSPawnDelay;
+
+    [Header("References")]
+    public GameObject[] gameObjects;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Invoke("Spawn", Random.Range(minSPawnDelay, maxSPawnDelay));
+    }
+
+    void Spawn()
+    {
+        var randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
+        Instantiate(randomObject, transform.position, Quaternion.identity);
+        Invoke("Spawn", Random.Range(minSPawnDelay, maxSPawnDelay)); // 인복은 일정 시간 후 호출
+    }
+}
