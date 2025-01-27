18cd1942f2eb9c6ad1c5973a8441db9c9410ccae
Alisser <gnlrjswjd@naver.com>
2025-01-27 18:31:08 +0900

    건물 사라지게

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Mover : MonoBehaviour
+{
+    [Header("Settings")]
+    public float moveSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        
+    }
+}
